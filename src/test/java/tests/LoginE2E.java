f2f0601c6ce20db0584eb67a15e8540cc5cb69d5
esimakova1984 <esimakova1984@gmail.com>
2023-02-01 15:43:39 +0200

    added

--- src/test/java/tests/LoginE2E.java
+++ src/test/java/tests/LoginE2E.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
         User user1 = User.builder()
                 .organization("ekaterina1")
-                .userName("admin8")
+                .userName("admin9")
                 .password("Qwerty123")
                 .build();
          User user2 = User.builder()
@@ -33,20 +33,20 @@ import org.testng.annotations.Test;
             app.getHelperLogin().signIn(user1);
             logger.info("The login form is filled with data: " + user1);
             Assert.assertTrue(app.getHelperLogin().isSignInSiccess());
-//            logger.info("The First Login form is opened");
-//            app.getHelperLogin().fillFirstLoginForm(userFirstLoginForm);
-//            logger.info("The First Login form is filled with data: " + userFirstLoginForm + ". Predefined views page is opened.");
-//            app.getHelperLogin().logOut();
-//            logger.info("The user logged out");
-//            Assert.assertTrue(app.getHelperLogin().isOrganizationfieldPresent());
-//            logger.info("The Login form for new organization is opened");
-//            app.getHelperLogin().signIn(user2);
-//            logger.info("The login form is filled with data: " + user2);
-//            Assert.assertTrue(app.getHelperLogin().isSignInSiccess());
-//            logger.info("The First Login form is opened");
-//            app.getHelperLogin().fillFirstLoginForm(userFirstLoginForm);
-//            logger.info("The First Login form is filled with data: " + userFirstLoginForm + ". Predefined views page is opened.");
-//            logger.info("Test passed successfully");
+            logger.info("The First Login form is opened");
+            app.getHelperLogin().fillFirstLoginForm(userFirstLoginForm);
+            logger.info("The First Login form is filled with data: " + userFirstLoginForm + ". Predefined views page is opened.");
+            app.getHelperLogin().logOut();
+            logger.info("The user logged out");
+            Assert.assertTrue(app.getHelperLogin().isOrganizationfieldPresent());
+            logger.info("The Login form for new organization is opened");
+            app.getHelperLogin().signIn(user2);
+            logger.info("The login form is filled with data: " + user2);
+            Assert.assertTrue(app.getHelperLogin().isSignInSiccess());
+            logger.info("The First Login form is opened");
+            app.getHelperLogin().fillFirstLoginForm(userFirstLoginForm);
+            logger.info("The First Login form is filled with data: " + userFirstLoginForm + ". Predefined views page is opened.");
+            logger.info("Test passed successfully");
 
         }
 
